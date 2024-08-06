b8cb7f0b16cda2b867c33db09fade10c0df8b9e0
sahil99811 <sp220253@gmail.com>
2024-08-06 23:40:57 +0530

    third commit

--- src/apis/Scrape.js
+++ src/apis/Scrape.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const React_App_Backend_URL=process.env.React_App_Backend_Url
 export const getScrapping=async (domain)=>{
     try {
-        console.log(React_App_Backend_URL);
+        console.log("backend url",React_App_Backend_URL);
         const result=await axios.get(`${React_App_Backend_URL}/scrape?id=${domain}`,{
             validateStatus(status) {
                 return status === 200 || status === 400; // Only resolve these status codes
